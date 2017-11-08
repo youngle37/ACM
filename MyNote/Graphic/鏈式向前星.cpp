89f73d6c74d5ae8ff54ba956f61b138d9c429fae
YoungLe <aa258163@gmail.com>
2017-11-08 13:49:57 +0800

    Finish MyNote/鏈式向前星.cpp

---
+++ MyNote/Graphic/鏈式向前星.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#define _ ios_base::sync_with_stdio(false);cin.tie(0);
+
+#define N 100
+
+using namespace std;
+
+struct Edge{
+    int to;
+    int w;
+    int next;
+} edge[N * 2];
+
+int cnt_edge = 0;
+int head[N * 2];
+
+void add_edge(int from, int to, int w){
+    edge[cnt_edge].to = to;
+    edge[cnt_edge].w = w;
+    edge[cnt_edge].next = head[from];
+    head[from] = cnt_edge++;
+}
+
+int main(){
+    _
+
+    memset(head, -1, sizeof(head));
+
+    int node_number;
+    cout << "Input node relation number: ";
+    cin >> node_number;
+
+    cout << "Input the node relation and weight: " << '\n';
+    int from , to, w;
+    for(int i=0;i<node_number;++i){
+        cin >> from >> to >> w;
+        add_edge(from, to, w);
+    }
+
+
+    int node;
+    while(true){
+        cout << "Input the node you want to traversal its neighbors:";
+        cin >> node;
+
+        for(int i=head[node]; ~i ; i=edge[i].next) // when i != -1 then continue
+            cout << edge[i].to << ' ';
+
+        cout << '\n';
+    }
+
+    return 0;
+}
