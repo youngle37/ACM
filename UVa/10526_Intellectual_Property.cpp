1b14bace43a32f7bfe52bd8767edb0aea70af4a8
YoungLe <aa258163@gmail.com>
2019-07-25 13:11:02 +0800

    Modify indentation

--- UVa/10526_Intellectual_Property.cpp
+++ UVa/10526_Intellectual_Property.cpp
@@ -22,75 +22,76 @@ int v[155555];
 
 struct Ans {
     int len, pos;
-    Ans(){};
-    Ans(int len, int pos){
+    Ans() {};
+    Ans(int len, int pos) {
         this->len = len;
         this->pos = pos;
     }
 } ans[155555];
 
 bool cmp(Ans a, Ans b) {
-	if (a.len == b.len) return a.pos < b.pos;
-	return a.len > b.len;
+    if (a.len == b.len) return a.pos < b.pos;
+    return a.len > b.len;
 }
 
-void input(){
+void input() {
     n = 0;
     an = 0;
 
     gets(str);
-    while(gets(str)){
+    while(gets(str)) {
         if(strcmp(str, "END TDP CODEBASE") == 0)
             break;
 
         int len = strlen(str);
         str[len] = '\n';
-        for(int i=0; i<=len; i++)
+        for(int i = 0; i <= len; i++)
             s[n++] = str[i];
     }
 
     tdp = n;
     s[n++] = 257;
-    
+
     gets(str);
-    while(gets(str)){
+    while(gets(str)) {
         if(strcmp(str, "END JCN CODEBASE") == 0)
             break;
 
         int len = strlen(str);
         str[len] = '\n';
-        for(int i=0; i<=len; i++)
+        for(int i = 0; i <= len; i++)
             s[n++] = str[i];
     }
 
     s[n] = 0;
 }
 
-void get_SA(int m){
+void get_SA(int m) {
     n++;
 
     int *x = t;
     int *y = t2;
 
     /* Counting Sort */
-    for(int i=0; i<m; i++) c[i] = 0;            // 所有ASCII歸零
-    for(int i=0; i<n; i++) c[x[i] = s[i]]++;    // 計算每個ASCII有多少個，順便把 x 初始為 s
-    for(int i=1; i<m; i++) c[i] += c[i-1];      // 將 c 往後累加下去
-    for(int i=n-1; i>=0; i--) sa[--c[x[i]]] = i;// 因為是 0-base 所以先減再放
+    for(int i = 0; i < m; i++) c[i] = 0;        // 所有ASCII歸零
+    for(int i = 0; i < n; i++) c[x[i] = s[i]]++; // 計算每個ASCII有多少個，順便把 x 初始為 s
+    for(int i = 1; i < m; i++) c[i] += c[i - 1]; // 將 c 往後累加下去
+    for(int i = n - 1; i >= 0; i--) sa[--c[x[i]]] = i; // 因為是 0-base 所以先減再放
 
     /* Doubling Algorithm */
-    for(int k=1; k<=n; k<<1){
+    for(int k = 1; k <= n; k << 1) {
         int p = 0;
-        for(int i=n-k; i<n; i++) y[p++] = i;
-        for(int i=0; i<n; i++) if (sa[i] >= k) y[p++] = sa[i] - k;
-        for(int i=0; i<m; i++) c[i] = 0;
-        for(int i=0; i<n; i++) c[x[y[i]]]++;
-        for(int i=1; i<m; i++) c[i] += c[i-1];
-        for(int i=n-1; i>=0; i--) sa[--c[x[y[i]]]] = y[i];
+        for(int i = n - k; i < n; i++) y[p++] = i;
+        for(int i = 0; i < n; i++) if (sa[i] >= k) y[p++] = sa[i] - k;
+        for(int i = 0; i < m; i++) c[i] = 0;
+        for(int i = 0; i < n; i++) c[x[y[i]]]++;
+        for(int i = 1; i < m; i++) c[i] += c[i - 1];
+        for(int i = n - 1; i >= 0; i--) sa[--c[x[y[i]]]] = y[i];
         swap(x, y);
-        p = 1; x[sa[0]] = 0;
-        for(int i=1; i<n; i++)
-            x[sa[i]] = (y[sa[i-1]] == y[sa[i]] && y[sa[i-1] + k] == y[sa[i] + k]) ? p - 1 : p++;
+        p = 1;
+        x[sa[0]] = 0;
+        for(int i = 1; i < n; i++)
+            x[sa[i]] = (y[sa[i - 1]] == y[sa[i]] && y[sa[i - 1] + k] == y[sa[i] + k]) ? p - 1 : p++;
         if(p >= n) break;
         m = p;
     }
@@ -99,10 +100,10 @@ void get_SA(int m){
 
 }
 
-void getHeight(){
+void getHeight() {
     int k = 0;
-    for(int i=1; i<=n; i++) Rank[sa[i]] = i;
-    for(int i=0; i<n; i++){
+    for(int i = 1; i <= n; i++) Rank[sa[i]] = i;
+    for(int i = 0; i < n; i++) {
         if(k) k--;
         int j = sa[Rank[i] - 1];
         while(s[i + k] == s[j + k]) k++;
@@ -110,54 +111,54 @@ void getHeight(){
     }
 }
 
-void solve(){
+void solve() {
     input();
     get_SA(258);
     getHeight();
     memset(v, 0, sizeof(v));
 
     int MIN = -1;
-    for(int i=1; i<=n; i++){
+    for(int i = 1; i <= n; i++) {
         if(sa[i] < tdp) MIN = INF;
-        else if(sa[i] > tdp){
+        else if(sa[i] > tdp) {
             if(MIN == -1) continue;
             MIN = min(height[i], MIN);
-            v[sa[i] - tdp - 1] = max(MIN, v[sa[i] - tdp -1]);
+            v[sa[i] - tdp - 1] = max(MIN, v[sa[i] - tdp - 1]);
         }
     }
 
     MIN = -1;
-    for(int i=n; i>=1; i--){
+    for(int i = n; i >= 1; i--) {
         if(sa[i] < tdp) MIN = INF;
-        else if(sa[i] > tdp){
+        else if(sa[i] > tdp) {
             if(MIN == -1) continue;
-            MIN = min(height[i+1], MIN);
+            MIN = min(height[i + 1], MIN);
             v[sa[i] - tdp - 1] = max(MIN, v[sa[i] - tdp - 1]);
         }
     }
 
     int r = -1;
-    for(int i=0; i<n-tdp; i++){
+    for(int i = 0; i < n - tdp; i++) {
         if(i + v[i] <= r) continue;
         if(v[i] == 0) continue;
         ans[an++] = Ans(v[i], i);
         r = i + v[i];
     }
 
-    sort(ans, ans+an, cmp);
+    sort(ans, ans + an, cmp);
 
-    for(int i=0; i<min(an, k); i++){
+    for(int i = 0; i < min(an, k); i++) {
         printf("INFRINGING SEGMENT %d LENGTH %d POSITION %d\n", i + 1, ans[i].len, ans[i].pos);
         for (int j = ans[i].pos + tdp + 1; j < ans[i].pos + tdp + 1 + ans[i].len; j++)
-                printf("%c", s[j]);
+            printf("%c", s[j]);
         printf("\n");
     }
 }
 
-int main(){
+int main() {
     int t = 0;
     bool what = false;
-    while(~scanf("%d\n", &k) && k){
+    while(~scanf("%d\n", &k) && k) {
         if(what)
             printf("\n");
         else
